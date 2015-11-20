66b8eebb6ee3efd02dd951707f079076f9caa75f
mohamedbenkirane <mbenkirane@enseirb-matmeca.fr>
2015-11-20 14:55:38 +0100

    Projet CPP

---
+++ CppApp/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mohamedbenkirane
+ *
+ * Created on 20 novembre 2015, 14:54
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
